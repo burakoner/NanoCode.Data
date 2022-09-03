101c927a29857c6c58f424b845d2d5ee8778b0b2
Burak Öner <info@burakoner.com>
2022-09-03 13:33:19 +0300

    Version 1.0.2

--- src/NanoCode.Data/Security/CryptoMethods.cs
+++ src/NanoCode.Data/Security/CryptoMethods.cs
@@ -6,7 +6,7 @@ using System.Text;
 
 namespace NanoCode.Data.Security
 {
-    public static class Cryptology
+    public static class CryptoMethods
     {
         /// <summary>
         /// Encrypt a byte array into a byte array using a key and an IV 
@@ -18,15 +18,15 @@ namespace NanoCode.Data.Security
         public static byte[] Encrypt(byte[] clearData, byte[] Key, byte[] IV)
         {
             // Create a MemoryStream to accept the encrypted bytes 
-            MemoryStream ms = new MemoryStream();
-            Rijndael alg = Rijndael.Create();
+            var ms = new MemoryStream();
+            var alg = Rijndael.Create();
             alg.Key = Key;
             alg.IV = IV;
-            CryptoStream cs = new CryptoStream(ms, alg.CreateEncryptor(), CryptoStreamMode.Write);
+            var cs = new CryptoStream(ms, alg.CreateEncryptor(), CryptoStreamMode.Write);
             cs.Write(clearData, 0, clearData.Length);
             cs.Close();
 
-            byte[] encryptedData = ms.ToArray();
+            var encryptedData = ms.ToArray();
             return encryptedData;
         }
 
@@ -38,9 +38,9 @@ namespace NanoCode.Data.Security
         /// <returns></returns>
         public static string Encrypt(string clearText, string Password)
         {
-            byte[] clearBytes = Encoding.Unicode.GetBytes(clearText);
-            PasswordDeriveBytes pdb = new PasswordDeriveBytes(Password, new byte[] { 0x49, 0x76, 0x61, 0x6e, 0x20, 0x4d, 0x65, 0x64, 0x76, 0x65, 0x64, 0x65, 0x76 });
-            byte[] encryptedData = Encrypt(clearBytes, pdb.GetBytes(32), pdb.GetBytes(16));
+            var clearBytes = Encoding.Unicode.GetBytes(clearText);
+            var pdb = new PasswordDeriveBytes(Password, new byte[] { 0x49, 0x76, 0x61, 0x6e, 0x20, 0x4d, 0x65, 0x64, 0x76, 0x65, 0x64, 0x65, 0x76 });
+            var encryptedData = Encrypt(clearBytes, pdb.GetBytes(32), pdb.GetBytes(16));
             return Convert.ToBase64String(encryptedData);
         }
 
@@ -52,7 +52,7 @@ namespace NanoCode.Data.Security
         /// <returns></returns>
         public static byte[] Encrypt(byte[] clearData, string Password)
         {
-            PasswordDeriveBytes pdb = new PasswordDeriveBytes(Password, new byte[] { 0x49, 0x76, 0x61, 0x6e, 0x20, 0x4d, 0x65, 0x64, 0x76, 0x65, 0x64, 0x65, 0x76 });
+            var pdb = new PasswordDeriveBytes(Password, new byte[] { 0x49, 0x76, 0x61, 0x6e, 0x20, 0x4d, 0x65, 0x64, 0x76, 0x65, 0x64, 0x65, 0x76 });
             return Encrypt(clearData, pdb.GetBytes(32), pdb.GetBytes(16));
         }
 
@@ -67,15 +67,15 @@ namespace NanoCode.Data.Security
         {
             try
             {
-                FileStream fsIn = new FileStream(fileIn, FileMode.Open, FileAccess.Read);
-                FileStream fsOut = new FileStream(fileOut, FileMode.OpenOrCreate, FileAccess.Write);
-                PasswordDeriveBytes pdb = new PasswordDeriveBytes(Password, new byte[] { 0x49, 0x76, 0x61, 0x6e, 0x20, 0x4d, 0x65, 0x64, 0x76, 0x65, 0x64, 0x65, 0x76 });
-                Rijndael alg = Rijndael.Create();
+                var fsIn = new FileStream(fileIn, FileMode.Open, FileAccess.Read);
+                var fsOut = new FileStream(fileOut, FileMode.OpenOrCreate, FileAccess.Write);
+                var pdb = new PasswordDeriveBytes(Password, new byte[] { 0x49, 0x76, 0x61, 0x6e, 0x20, 0x4d, 0x65, 0x64, 0x76, 0x65, 0x64, 0x65, 0x76 });
+                var alg = Rijndael.Create();
                 alg.Key = pdb.GetBytes(32);
                 alg.IV = pdb.GetBytes(16);
-                CryptoStream cs = new CryptoStream(fsOut, alg.CreateEncryptor(), CryptoStreamMode.Write);
-                int bufferLen = 4096;
-                byte[] buffer = new byte[bufferLen];
+                var cs = new CryptoStream(fsOut, alg.CreateEncryptor(), CryptoStreamMode.Write);
+                var bufferLen = 4096;
+                var buffer = new byte[bufferLen];
                 int bytesRead;
                 do
                 {
@@ -108,13 +108,13 @@ namespace NanoCode.Data.Security
         /// <returns></returns>
         public static byte[] Decrypt(byte[] cipherData, byte[] Key, byte[] IV)
         {
-            MemoryStream ms = new MemoryStream();
+            var ms = new MemoryStream();
             try
             {
-                Rijndael alg = Rijndael.Create();
+                var alg = Rijndael.Create();
                 alg.Key = Key;
                 alg.IV = IV;
-                CryptoStream cs = new CryptoStream(ms, alg.CreateDecryptor(), CryptoStreamMode.Write);
+                var cs = new CryptoStream(ms, alg.CreateDecryptor(), CryptoStreamMode.Write);
                 cs.Write(cipherData, 0, cipherData.Length);
                 cs.Close();
             }
@@ -122,7 +122,7 @@ namespace NanoCode.Data.Security
             {
 
             }
-            byte[] decryptedData = ms.ToArray();
+            var decryptedData = ms.ToArray();
 
             return decryptedData;
         }
@@ -135,9 +135,9 @@ namespace NanoCode.Data.Security
         /// <returns></returns>
         public static string Decrypt(string cipherText, string Password)
         {
-            byte[] cipherBytes = Convert.FromBase64String(cipherText);
-            PasswordDeriveBytes pdb = new PasswordDeriveBytes(Password, new byte[] { 0x49, 0x76, 0x61, 0x6e, 0x20, 0x4d, 0x65, 0x64, 0x76, 0x65, 0x64, 0x65, 0x76 });
-            byte[] decryptedData = Decrypt(cipherBytes, pdb.GetBytes(32), pdb.GetBytes(16));
+            var cipherBytes = Convert.FromBase64String(cipherText);
+            var pdb = new PasswordDeriveBytes(Password, new byte[] { 0x49, 0x76, 0x61, 0x6e, 0x20, 0x4d, 0x65, 0x64, 0x76, 0x65, 0x64, 0x65, 0x76 });
+            var decryptedData = Decrypt(cipherBytes, pdb.GetBytes(32), pdb.GetBytes(16));
             return Encoding.Unicode.GetString(decryptedData);
         }
 
@@ -149,7 +149,7 @@ namespace NanoCode.Data.Security
         /// <returns></returns>
         public static byte[] Decrypt(byte[] cipherData, string Password)
         {
-            PasswordDeriveBytes pdb = new PasswordDeriveBytes(Password, new byte[] { 0x49, 0x76, 0x61, 0x6e, 0x20, 0x4d, 0x65, 0x64, 0x76, 0x65, 0x64, 0x65, 0x76 });
+            var pdb = new PasswordDeriveBytes(Password, new byte[] { 0x49, 0x76, 0x61, 0x6e, 0x20, 0x4d, 0x65, 0x64, 0x76, 0x65, 0x64, 0x65, 0x76 });
             return Decrypt(cipherData, pdb.GetBytes(32), pdb.GetBytes(16));
         }
 
@@ -165,15 +165,15 @@ namespace NanoCode.Data.Security
             try
             {
                 // First we are going to open the file streams 
-                FileStream fsIn = new FileStream(fileIn, FileMode.Open, FileAccess.Read);
-                FileStream fsOut = new FileStream(fileOut, FileMode.OpenOrCreate, FileAccess.Write);
-                PasswordDeriveBytes pdb = new PasswordDeriveBytes(Password, new byte[] { 0x49, 0x76, 0x61, 0x6e, 0x20, 0x4d, 0x65, 0x64, 0x76, 0x65, 0x64, 0x65, 0x76 });
-                Rijndael alg = Rijndael.Create();
+                var fsIn = new FileStream(fileIn, FileMode.Open, FileAccess.Read);
+                var fsOut = new FileStream(fileOut, FileMode.OpenOrCreate, FileAccess.Write);
+                var pdb = new PasswordDeriveBytes(Password, new byte[] { 0x49, 0x76, 0x61, 0x6e, 0x20, 0x4d, 0x65, 0x64, 0x76, 0x65, 0x64, 0x65, 0x76 });
+                var alg = Rijndael.Create();
                 alg.Key = pdb.GetBytes(32);
                 alg.IV = pdb.GetBytes(16);
-                CryptoStream cs = new CryptoStream(fsOut, alg.CreateDecryptor(), CryptoStreamMode.Write);
-                int bufferLen = 4096;
-                byte[] buffer = new byte[bufferLen];
+                var cs = new CryptoStream(fsOut, alg.CreateDecryptor(), CryptoStreamMode.Write);
+                var bufferLen = 4096;
+                var buffer = new byte[bufferLen];
                 int bytesRead;
 
                 do
@@ -203,7 +203,7 @@ namespace NanoCode.Data.Security
         /// </summary>
         public enum HashType
         {
-            HMAC, HMACMD5, HMACSHA1, HMACSHA256, HMACSHA384, HMACSHA512, MACTripleDES, MD5, RIPEMD160, SHA1, SHA256, SHA384, SHA512
+            HMAC, HMACMD5, HMACSHA1, HMACSHA256, HMACSHA384, HMACSHA512, /*MACTripleDES,*/ MD5, /*RIPEMD160,*/ SHA1, SHA256, SHA384, SHA512
         }
 
         private static byte[] GetHash(string Source, HashType hash)
@@ -229,16 +229,16 @@ namespace NanoCode.Data.Security
 
                 case HashType.HMACSHA512:
                     return HMACSHA512.Create().ComputeHash(inputBytes);
-                    /*
-                case HashType.MACTripleDES:
-                    return MACTripleDES.Create().ComputeHash(inputBytes);
-                    */
+                /*
+            case HashType.MACTripleDES:
+                return MACTripleDES.Create().ComputeHash(inputBytes);
+                */
                 case HashType.MD5:
                     return MD5.Create().ComputeHash(inputBytes);
-                    /*
-                case HashType.RIPEMD160:
-                    return RIPEMD160.Create().ComputeHash(inputBytes);
-                    */
+                /*
+            case HashType.RIPEMD160:
+                return RIPEMD160.Create().ComputeHash(inputBytes);
+                */
                 case HashType.SHA1:
                     return SHA1.Create().ComputeHash(inputBytes);
 
@@ -266,8 +266,8 @@ namespace NanoCode.Data.Security
         {
             try
             {
-                byte[] hash = GetHash(Source, hashType);
-                StringBuilder ret = new StringBuilder();
+                var hash = GetHash(Source, hashType);
+                var ret = new StringBuilder();
 
                 for (int i = 0; i < hash.Length; i++)
                     ret.Append(hash[i].ToString("x2"));
@@ -282,12 +282,12 @@ namespace NanoCode.Data.Security
 
         public static string GetMD5HashFromFile(string fileName)
         {
-            FileStream file = new FileStream(fileName, FileMode.Open);
-            MD5 md5 = new MD5CryptoServiceProvider();
-            byte[] retVal = md5.ComputeHash(file);
+            var file = new FileStream(fileName, FileMode.Open);
+            var md5 = new MD5CryptoServiceProvider();
+            var retVal = md5.ComputeHash(file);
             file.Close();
 
-            StringBuilder sb = new StringBuilder();
+            var sb = new StringBuilder();
             for (int i = 0; i < retVal.Length; i++)
             {
                 sb.Append(retVal[i].ToString("x2"));
@@ -297,8 +297,8 @@ namespace NanoCode.Data.Security
 
         public static string Hmac256(string message, string secret)
         {
-            Encoding encoding = Encoding.UTF8;
-            using (HMACSHA256 hmac = new HMACSHA256(encoding.GetBytes(secret)))
+            var encoding = Encoding.UTF8;
+            using (var hmac = new HMACSHA256(encoding.GetBytes(secret)))
             {
                 var msg = encoding.GetBytes(message);
                 var hash = hmac.ComputeHash(msg);
@@ -308,8 +308,8 @@ namespace NanoCode.Data.Security
 
         public static string Hmac384(string message, string secret)
         {
-            Encoding encoding = Encoding.UTF8;
-            using (HMACSHA384 hmac = new HMACSHA384(encoding.GetBytes(secret)))
+            var encoding = Encoding.UTF8;
+            using (var hmac = new HMACSHA384(encoding.GetBytes(secret)))
             {
                 var msg = encoding.GetBytes(message);
                 var hash = hmac.ComputeHash(msg);
@@ -319,8 +319,8 @@ namespace NanoCode.Data.Security
 
         public static string Hmac512(string message, string secret)
         {
-            Encoding encoding = Encoding.UTF8;
-            using (HMACSHA512 hmac = new HMACSHA512(encoding.GetBytes(secret)))
+            var encoding = Encoding.UTF8;
+            using (var hmac = new HMACSHA512(encoding.GetBytes(secret)))
             {
                 var msg = encoding.GetBytes(message);
                 var hash = hmac.ComputeHash(msg);
@@ -332,16 +332,16 @@ namespace NanoCode.Data.Security
         {
             try
             {
-                byte[] clearBytes = System.Text.Encoding.Unicode.GetBytes(Data);
-                PasswordDeriveBytes pdb = new PasswordDeriveBytes(Key, new byte[] { 0x00, 0x01, 0x02, 0x1C, 0x1D, 0x1E, 0x03, 0x04, 0x05, 0x0F, 0x20, 0x21, 0xAD, 0xAF, 0xA4 });
-                MemoryStream ms = new MemoryStream();
-                Rijndael alg = Rijndael.Create();
+                var clearBytes = Encoding.Unicode.GetBytes(Data);
+                var pdb = new PasswordDeriveBytes(Key, new byte[] { 0x00, 0x01, 0x02, 0x1C, 0x1D, 0x1E, 0x03, 0x04, 0x05, 0x0F, 0x20, 0x21, 0xAD, 0xAF, 0xA4 });
+                var ms = new MemoryStream();
+                var alg = Rijndael.Create();
                 alg.Key = pdb.GetBytes(16);
                 alg.IV = pdb.GetBytes(16);
-                CryptoStream cs = new CryptoStream(ms, alg.CreateEncryptor(), CryptoStreamMode.Write);
+                var cs = new CryptoStream(ms, alg.CreateEncryptor(), CryptoStreamMode.Write);
                 cs.Write(clearBytes, 0, clearBytes.Length);
                 cs.Close();
-                byte[] encryptedData = ms.ToArray();
+                var encryptedData = ms.ToArray();
                 return Convert.ToBase64String(encryptedData);
             }
             catch (Exception Ex)
@@ -355,17 +355,17 @@ namespace NanoCode.Data.Security
         {
             try
             {
-                byte[] clearBytes = Convert.FromBase64String(Data);
-                PasswordDeriveBytes pdb = new PasswordDeriveBytes(Key, new byte[] { 0x00, 0x01, 0x02, 0x1C, 0x1D, 0x1E, 0x03, 0x04, 0x05, 0x0F, 0x20, 0x21, 0xAD, 0xAF, 0xA4 });
-                MemoryStream ms = new MemoryStream();
-                Rijndael alg = Rijndael.Create();
+                var clearBytes = Convert.FromBase64String(Data);
+                var pdb = new PasswordDeriveBytes(Key, new byte[] { 0x00, 0x01, 0x02, 0x1C, 0x1D, 0x1E, 0x03, 0x04, 0x05, 0x0F, 0x20, 0x21, 0xAD, 0xAF, 0xA4 });
+                var ms = new MemoryStream();
+                var alg = Rijndael.Create();
                 alg.Key = pdb.GetBytes(16);
                 alg.IV = pdb.GetBytes(16);
-                CryptoStream cs = new CryptoStream(ms, alg.CreateDecryptor(), CryptoStreamMode.Write);
+                var cs = new CryptoStream(ms, alg.CreateDecryptor(), CryptoStreamMode.Write);
                 cs.Write(clearBytes, 0, clearBytes.Length);
                 cs.Close();
-                byte[] decryptedData = ms.ToArray();
-                return System.Text.Encoding.Unicode.GetString(decryptedData);
+                var decryptedData = ms.ToArray();
+                return Encoding.Unicode.GetString(decryptedData);
             }
             catch (Exception Ex)
             {
@@ -378,16 +378,16 @@ namespace NanoCode.Data.Security
         {
             try
             {
-                byte[] clearBytes = System.Text.Encoding.Unicode.GetBytes(Data);
-                PasswordDeriveBytes pdb = new PasswordDeriveBytes(Key, new byte[] { 0x00, 0x01, 0x02, 0x1C, 0x1D, 0x1E, 0x03, 0x04, 0x05, 0x0F, 0x20, 0x21, 0xAD, 0xAF, 0xA4 });
-                MemoryStream ms = new MemoryStream();
-                Rijndael alg = Rijndael.Create();
+                var clearBytes = Encoding.Unicode.GetBytes(Data);
+                var pdb = new PasswordDeriveBytes(Key, new byte[] { 0x00, 0x01, 0x02, 0x1C, 0x1D, 0x1E, 0x03, 0x04, 0x05, 0x0F, 0x20, 0x21, 0xAD, 0xAF, 0xA4 });
+                var ms = new MemoryStream();
+                var alg = Rijndael.Create();
                 alg.Key = pdb.GetBytes(32);
                 alg.IV = pdb.GetBytes(16);
-                CryptoStream cs = new CryptoStream(ms, alg.CreateEncryptor(), CryptoStreamMode.Write);
+                var cs = new CryptoStream(ms, alg.CreateEncryptor(), CryptoStreamMode.Write);
                 cs.Write(clearBytes, 0, clearBytes.Length);
                 cs.Close();
-                byte[] encryptedData = ms.ToArray();
+                var encryptedData = ms.ToArray();
                 return Convert.ToBase64String(encryptedData);
             }
             catch (Exception Ex)
@@ -401,17 +401,17 @@ namespace NanoCode.Data.Security
         {
             try
             {
-                byte[] clearBytes = Convert.FromBase64String(Data);
-                PasswordDeriveBytes pdb = new PasswordDeriveBytes(Key, new byte[] { 0x00, 0x01, 0x02, 0x1C, 0x1D, 0x1E, 0x03, 0x04, 0x05, 0x0F, 0x20, 0x21, 0xAD, 0xAF, 0xA4 });
-                MemoryStream ms = new MemoryStream();
-                Rijndael alg = Rijndael.Create();
+                var clearBytes = Convert.FromBase64String(Data);
+                var pdb = new PasswordDeriveBytes(Key, new byte[] { 0x00, 0x01, 0x02, 0x1C, 0x1D, 0x1E, 0x03, 0x04, 0x05, 0x0F, 0x20, 0x21, 0xAD, 0xAF, 0xA4 });
+                var ms = new MemoryStream();
+                var alg = Rijndael.Create();
                 alg.Key = pdb.GetBytes(32);
                 alg.IV = pdb.GetBytes(16);
-                CryptoStream cs = new CryptoStream(ms, alg.CreateDecryptor(), CryptoStreamMode.Write);
+                var cs = new CryptoStream(ms, alg.CreateDecryptor(), CryptoStreamMode.Write);
                 cs.Write(clearBytes, 0, clearBytes.Length);
                 cs.Close();
-                byte[] decryptedData = ms.ToArray();
-                return System.Text.Encoding.Unicode.GetString(decryptedData);
+                var decryptedData = ms.ToArray();
+                return Encoding.Unicode.GetString(decryptedData);
             }
             catch (Exception Ex)
             {
@@ -419,11 +419,11 @@ namespace NanoCode.Data.Security
                 return "Failed!";
             }
         }
-        
-        public static string ConvertStringToHex(String input, System.Text.Encoding encoding)
+
+        public static string ConvertStringToHex(string input, Encoding encoding)
         {
-            Byte[] stringBytes = encoding.GetBytes(input);
-            StringBuilder sbBytes = new StringBuilder(stringBytes.Length * 2);
+            var stringBytes = encoding.GetBytes(input);
+            var sbBytes = new StringBuilder(stringBytes.Length * 2);
             foreach (byte b in stringBytes)
             {
                 sbBytes.AppendFormat("{0:X2}", b);
@@ -431,10 +431,10 @@ namespace NanoCode.Data.Security
             return sbBytes.ToString();
         }
 
-        public static string ConvertHexToString(String hexInput, System.Text.Encoding encoding)
+        public static string ConvertHexToString(string hexInput, Encoding encoding)
         {
-            int numberChars = hexInput.Length;
-            byte[] bytes = new byte[numberChars / 2];
+            var numberChars = hexInput.Length;
+            var bytes = new byte[numberChars / 2];
             for (int i = 0; i < numberChars; i += 2)
             {
                 bytes[i / 2] = Convert.ToByte(hexInput.Substring(i, 2), 16);
